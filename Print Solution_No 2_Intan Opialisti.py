32ec84db2f25eb8118fde6c0d5cea660f1fdae1d
Intanopialisti04 <114717130+Intanopialisti04@users.noreply.github.com>
2022-12-27 18:08:49 +0700

    Menambahkan
    import numpy as np
    import matplotlib.pyplot as plt
    
    alpha = np.radians(45)
    g = 9.8
    v0 = 10
    
    v0x = v0*np.cos(alpha)
    v0y = v0*np.sin(alpha)
    
    X = ((v0**2)*np.sin(2*alpha))/(2*g)
    print("Jarak Horizontal Maksimum = ",X," m")
    Y = ((v0**2)*(np.sin(alpha)**2))/(2*g)
    print("Jarak Vertikal Maksimum = ",Y," m")
    T = (2*v0*np.sin(alpha))/g
    print ("Waktu Mencapai Jarak Horizontal Maksimum = ",T," s")
    print("\n")
    
    t = np.arange(0.0, T, 0.01)
    y = v0y*t - 0.5*g*t**2
    x = v0x*t
    
    fig, ax = plt.subplots()
    ax.plot(x, y)
    ax.set(xlabel='x (m)', ylabel= 'y (m)' , title='Grafik Gerak Parabola')
    ax.grid()
    plt.show()
    '''Rumus Fokus Lensa
         1/f = (n-1)[1/R1 + 1/R2]
    '''
    n  = 1.5   
    R1 = 10   
    R2 = 13
    
    F = (n-1)*((1/R1)+(1/R2))
    F = 1/F
    
    print("-"*40)
    print("Jarak Fokus Lensa = ",F)

---
+++ Print Solution_No 2_Intan Opialisti.py
@@ -0,0 +1,12 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/acer/Videos/UAS_Praktikum Fisika Komputasi/No 2. Mmplot titik pada parabola dalam kondisi t tertentu.py
+Jarak Horizontal Maksimum =  5.1020408163265305  m
+Jarak Vertikal Maksimum =  2.5510204081632657  m
+Waktu Mencapai Jarak Horizontal Maksimum =  1.4430750636460152  s
+
+
+----------------------------------------
+Jarak Fokus Lensa =  11.304347826086955
+>>> 
